644aef3e71e8da597b6f69247edc16505050a532
CookieUniverse <coookieuniverse@gmail.com>
2024-09-15 18:42:06 +0100

    added the givenNumber command

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GIVEN_NUMBER  = "GN";
+    public static final String EQUAL_PHONE  = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,8 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String NUMBER_NOT_EXIST = "Phone number does not exist.";
+    public static final String NUMBER_SHARED = "There are contacts that share phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +56,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GIVEN_NUMBER:
+                    givenNumber(in,cBook);
+                    break;
+                case EQUAL_PHONE:
+                    equalPhone(in, cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +156,21 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void givenNumber(Scanner in, ContactBook cBook) {
+        int number;
+        number = in.nextInt(); in.nextLine();
+        if (cBook.hasContact(number)) {
+            Contact contact = cBook.getContactFromNumber(number);
+            System.out.println(contact.getName());
+        }
+        else {
+            System.out.println(NUMBER_NOT_EXIST);
+        }
+    }
+
+    private static void equalPhone(Scanner in, ContactBook cBook) {
+
     }
 }
